21989effc06c35f3afc6b4fd4fe8022bc1554148
saeyshakumar <87544737+saeyshakumar@users.noreply.github.com>
2021-09-19 12:08:07 -0400

    Add files via upload

---
+++ js/Blower.js
@@ -0,0 +1,26 @@
+class Blower
+{
+  constructor(x, y, w,h) 
+  {
+    let options = {
+    isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+
+  show() {
+    let pos = this.body.position;
+    var angle = this.body.angle
+    push();
+    translate (pos.x,pos.y)
+    rotate (angle)
+    rectMode(CENTER);
+    fill ("brown")
+    rect(0,0, this.w, this.h);
+    pop();
+  }
+}
